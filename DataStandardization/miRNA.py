020fac7e863ac5850fffd7b527050bbed9e47ac8
nathanmella <nathanmell@gmail.com>
2019-05-07 15:51:49 -0600

    Finished Scripts for standardizing miRNA

--- DataStandardization/miRNA.py
+++ DataStandardization/miRNA.py
@@ -1,26 +1,98 @@
 import pandas as pd
 import sys
-PatientID = []
-Check = {}
-duplicate_indexes = []
-with open(sys.argv[1]) as input:
-    first_line = input.readline()
-    first_line = first_line.strip('\n')
-    Input = first_line.split('\t')
-    index = 0
-    for x in Input:
-        if x in PatientID:
-            myint = PatientID.index(x)
-            print()
-            print(PatientID[myint])
-            duplicate_indexes.append(PatientID.index(x))
-            duplicate_indexes.append(index)
-        if x.endswith("01"):
-            PatientID.append(x)
-        index +=1
-df = pd.read_csv(sys.argv[1], delimiter='\t', usecols=PatientID)
 
-for x in duplicate_indexes:
-    Check[x] = df[PatientID[x]]
-# print(df)
-    print(PatientID[x])
+RelevantTypes = ("Glioblastoma multiforme","Ovarian serous cystadenocarcinoma", "Lung squamous cell carcinoma",
+                 "Breast invasive carcinoma","Lung adenocarcinoma", "Prostate adenocarcinoma",
+                 "Skin Cutaneous Melanoma", "Colon adenocarcinoma", "Bladder Urothelial Carcinoma", "Sarcoma", "Kidney renal clear cell carcinoma")
+TSSDictionary = {}
+CancerDict = {"Glioblastoma multiforme": [],"Ovarian serous cystadenocarcinoma" : [], "Lung squamous cell carcinoma":[],
+                 "Breast invasive carcinoma":[],"Lung adenocarcinoma": [], "Prostate adenocarcinoma" : [],
+                 "Skin Cutaneous Melanoma": [], "Colon adenocarcinoma": [], "Bladder Urothelial Carcinoma": [], "Sarcoma": [],
+              "Kidney renal clear cell carcinoma": []}
+
+RelevantCodes = set()
+
+GM = "Glioblastoma multiforme"
+OSC = "Ovarian serous cystadenocarcinoma"
+LS = "Lung squamous cell carcinoma"
+BC = "Breast invasive carcinoma"
+LA = "Lung adenocarcinoma"
+PA = "Prostate adenocarcinoma"
+SC = "Skin Cutaneous Melanoma"
+CA = "Colon adenocarcinoma"
+BUC = "Bladder Urothelial Carcinoma"
+S = "Sarcoma"
+KIRC = "Kidney renal clear cell carcinoma"
+
+
+with open("TSS_CODES.tsv") as codes:
+    first_line = codes.readline()
+    for x in codes:
+        line = x.strip('\n').split('\t')
+        if line[2] in RelevantTypes:
+            RelevantCodes.add(line[0])
+            TSSDictionary[line[0]] = line[2]
+
+# So that the outputfile will have the correct names
+Abbreviations_Dict = {}
+with open("abreviations.tsv") as abr:
+    first_line = abr.readline()
+    for x in abr:
+        list = x.strip('\n').split('\t')
+        if list[1] in RelevantTypes:
+            Abbreviations_Dict[list[1]] = list[0]
+
+
+file_name = sys.argv[1]
+CancerPatientIDs = []
+Duplicate_Indexes = {}
+
+with open(sys.argv[1]) as input:
+    AllPatients= input.readline().strip('\n').split('\t')
+    CancerIndex = 0
+    for x in AllPatients:
+        if x in CancerPatientIDs:
+            first_duplicate = CancerPatientIDs.index(x)
+            second_duplicate = CancerIndex
+            Duplicate_Indexes[first_duplicate] = second_duplicate
+        if x.endswith("01") and x.split('-')[1] in RelevantCodes:
+            CancerPatientIDs.append(x)
+            tss = x.split('-')[1]
+            if TSSDictionary[tss] == GM:
+                CancerDict[GM].append(x)
+            elif TSSDictionary[tss] == OSC:
+                CancerDict[OSC].append(x)
+            elif TSSDictionary[tss] == BC:
+                CancerDict[BC].append(x)
+            elif TSSDictionary[tss] == LA:
+                CancerDict[LA].append(x)
+            elif TSSDictionary[tss] == PA:
+                CancerDict[PA].append(x)
+            elif TSSDictionary[tss] == SC:
+                CancerDict[SC].append(x)
+            elif TSSDictionary[tss] == CA:
+                CancerDict[CA].append(x)
+            elif TSSDictionary[tss] == BUC:
+                CancerDict[BUC].append(x)
+            elif TSSDictionary[tss] == S:
+                CancerDict[S].append(x)
+            elif TSSDictionary[tss] == KIRC:
+                CancerDict[S].append(x)
+            CancerIndex +=1
+
+df = pd.read_csv(sys.argv[1], delimiter='\t', usecols=CancerPatientIDs)[CancerPatientIDs]
+
+#take out repeats
+for x in Duplicate_Indexes:
+    val1 = x
+    val2 = Duplicate_Indexes[x]
+    print("First duplicate = " + CancerPatientIDs[val1] + " occured at index " + str(val1))
+    print("Second duplicate = " + CancerPatientIDs[val2] + " occured at index " + str(val2))
+    duplicate_df = df[CancerPatientIDs[x]] # grab the columns that repeat
+    series = duplicate_df.mean(axis=1) # average the duplicate values together
+    temp_df = pd.DataFrame({CancerPatientIDs[x]:series.values}) #turn the series into a DataFrame
+    df = df.drop(labels=[CancerPatientIDs[x]], axis=1) # take out all of the duplicate columns
+    df = pd.concat([df,temp_df], axis=1) # concate the average of the duplicate columns
+
+for x in CancerDict:
+    df[CancerDict[x]].to_csv(path_or_buf=('TCGA_' + Abbreviations_Dict[x] + '.ttsv'), sep='\t')
--- DataStandardization/miRNA.sh
+++ DataStandardization/miRNA.sh
@@ -4,7 +4,7 @@ fileName="pancanMiRs_EBadjOnProtocolPlatformWithoutRepsWithUnCorrectMiRs_08_04_1
 extention=".gz"
 unzippedFile=${fileName}${extention}
 
-if [ -f $fileName* ]
+if [ -e $fileName* ]
 then
     gunzip $unzippedFile
     echo IT WORKED!!!
@@ -15,6 +15,175 @@ else
     wget https://pancanatlas.xenahubs.net/download/${unzippedFile}
     bash miRNA.sh
 fi
+rm $fileName
+mv *.ttsv ../
+cd ../
+if [ -d "InputData" ]
+then
+    mv *.ttsv InputData/ 
+else
+    mkdir InputData 
+    mv *.ttsv InputData/ 
+fi
+
+cd InputData/
+
+Path=$(pwd)
+Files=${Path}/*
+
+echo Path
+echo $Files
 
 
+if [ -d "TCGA_BLCA/miRNA/" ]
+then
+    mv TCGA_BLCA.ttsv TCGA_BLCA/miRNA/
+else
+    if [ -d "TCGA_BLCA" ]
+    then
+        mkdir TCGA_BLCA/miRNA
+        mv TCGA_BLCA.ttsv TCGA_BLCA/miRNA/
+    else
+        mkdir TCGA_BLCA
+        mkdir TCGA_BLCA/miRNA
+        mv TCGA_BLCA.ttsv TCGA_BLCA/miRNA/
+    fi
+fi
+if [ -d "TCGA_COAD/miRNA/" ]
+then
+    mv TCGA_COAD.ttsv TCGA_COAD/miRNA/
+else
+    if [ -d "TCGA_COAD" ]
+    then
+        mkdir TCGA_COAD/miRNA
+        mv TCGA_COAD.ttsv TCGA_COAD/miRNA/
+    else
+        mkdir TCGA_COAD
+        mkdir TCGA_COAD/miRNA
+        mv TCGA_COAD.ttsv TCGA_COAD/miRNA/
+    fi
+fi
+if [ -d "TCGA_GBM/miRNA/" ]
+then
+    mv TCGA_GBM.ttsv TCGA_GBM/miRNA/
+else
+    if [ -d "TCGA_GBM" ]
+    then
+        mkdir TCGA_GBM/miRNA
+        mv TCGA_GBM.ttsv TCGA_GBM/miRNA/
+    else
+        mkdir TCGA_GBM
+        mkdir TCGA_GBM/miRNA
+        mv TCGA_GBM.ttsv TCGA_GBM/miRNA/
+    fi
+fi
+if [ -d "TCGA_KIRC/miRNA/" ]
+then
+    mv TCGA_KIRC.ttsv TCGA_KIRC/miRNA/
+else
+    if [ -d "TCGA_KIRC/" ]
+    then
+        mkdir TCGA_KIRC/miRNA
+        mv TCGA_KIRC.ttsv TCGA_KIRC/miRNA/
+    else
+        mkdir TCGA_KIRC
+        mkdir TCGA_KIRC/miRNA
+        mv TCGA_KIRC.ttsv TCGA_KIRC/miRNA/
+    fi
+fi
+if [ -d "TCGA_LUAD/miRNA/" ]
+then
+    mv TCGA_LUAD.ttsv TCGA_LUAD/miRNA/
+else
+    if [ -d "TCGA_LUAD" ]; then
+        mkdir TCGA_LUAD/miRNA
+        mv TCGA_LUAD.ttsv TCGA_LUAD/miRNA/
+    else
+        mkdir TCGA_LUAD
+        mkdir TCGA_LUAD/miRNA
+        mv TCGA_LUAD.ttsv TCGA_LUAD/miRNA/
+    fi
+fi
+if [ -d "TCGA_PRAD/miRNA/" ]
+then
+    mv TCGA_PRAD.ttsv TCGA_PRAD/miRNA/
+else
+    if [ -d "TCGA_PRAD" ]; then
+        mkdir TCGA_PRAD/miRNA
+        mv TCGA_PRAD.ttsv TCGA_PRAD/miRNA/
+    else
+        mkdir TCGA_PRAD
+        mkdir TCGA_PRAD/miRNA
+        mv TCGA_PRAD.ttsv TCGA_PRAD/miRNA/
+    fi
+fi
+if [ -d "TCGA_SARC/miRNA/" ]
+then
+    mv TCGA_SARC.ttsv TCGA_SARC/miRNA/
+else
+    if [ -d "TCGA_SARC/" ]; then
+        mkdir TCGA_SARC/miRNA
+        mv TCGA_SARC.ttsv TCGA_SARC/miRNA/
+    else
+        mkdir TCGA_SARC
+        mkdir TCGA_SARC/miRNA
+        mv TCGA_SARC.ttsv TCGA_SARC/miRNA/
+    fi
+fi
+if [ -d "TCGA_OV/miRNA/" ]
+then
+    mv TCGA_OV.ttsv TCGA_OV/miRNA/
+else
+    if [ -d "TCGA_OV/miRNA/" ]
+    then
+        mkdir TCGA_OV/miRNA
+        mv TCGA_OV.ttsv TCGA_OV/miRNA/
+    else
+        mkdir TCGA_OV
+        mkdir TCGA_OV/miRNA
+        mv TCGA_OV.ttsv TCGA_OV/miRNA/
+    fi
+fi
+if [ -d "TCGA_SKCM/miRNA/" ]
+then
+    mv TCGA_SKCM.ttsv TCGA_SKCM/miRNA/
+else
+    if [ -d "TCGA_SKCM/" ]
+    then
+        mkdir TCGA_SKCM/miRNA
+        mv TCGA_SKCM.ttsv TCGA_SKCM/miRNA/
+    else
+        mkdir TCGA_SKCM
+        mkdir TCGA_SKCM/miRNA
+        mv TCGA_SKCM.ttsv TCGA_SKCM/miRNA/
+    fi
+fi
+if [ -d "TCGA_BRCA/miRNA/" ]
+then
+    mv TCGA_BRCA.ttsv TCGA_BRCA/miRNA/
+else
+    if [ -d "TCGA_BRCA/" ]
+    then
+        mkdir TCGA_BRCA/miRNA
+        mv TCGA_BRCA.ttsv TCGA_BRCA/miRNA/
+    else
+        mkdir TCGA_BRCA
+        mkdir TCGA_BRCA/miRNA
+        mv TCGA_BRCA.ttsv TCGA_BRCA/miRNA/
+    fi
+fi
+if [ -d "TCGA_LUSC/miRNA/" ]
+then
+    mv TCGA_LUSC.ttsv TCGA_LUSC/miRNA/
+else
+    if [ -d "TCGA_LUSC/" ]
+    then
+        mkdir TCGA_LUSC/miRNA
+        mv TCGA_LUSC.ttsv TCGA_LUSC/miRNA/
+    else
+        mkdir TCGA_LUSC
+        mkdir TCGA_LUSC/miRNA
+        mv TCGA_LUSC.ttsv TCGA_LUSC/miRNA/
+    fi
+fi
 
