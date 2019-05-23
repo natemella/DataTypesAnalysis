64c80324b2d7379d8cdba9934225d7e5db491bd6
Tyler Heaton <tylerbheaton@gmail.com>
2019-05-22 20:33:34 -0600

    another message. I don't know why we need to do these always...

--- DataStandardization/CreateDNAmethDataCommands.py
+++ DataStandardization/CreateDNAmethDataCommands.py
@@ -2,7 +2,12 @@ with open("CancerTypes.txt") as input:
     output = open("DNA_MethylationFiles.sh", "w")
     for line in input:
         line = line.strip("\n")
-        output.write(f"Rscript Prepare_Methylation_Data.R "
+        output.write(f"mkdir -p output_Data\n"
+                     f"docker run --rm \\\n"
+                     f"-v `pwd`/output_Data:/output_Data \\\n"
+                     f"-v `pwd`:/Scripts \\\n"
+                     f"--user $(id -u):$(id -g) \\\n"
+                     f"rocker/tidyverse Rscript /Scripts/Prepare_Methylation_Data.R "
                      f"https://tcga.xenahubs.net/download/TCGA."
                      f"{line}.sampleMap/HumanMethylation450.gz {line}\n")
 
