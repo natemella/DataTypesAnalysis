0e3045f5c96e813628c365999e22ed48c39e5958
nathanmella <nathanmell@gmail.com>
2019-05-22 21:18:26 -0600

    I made my parser for miRNA and CNV faster

--- DataStandardization/DNA_Methylation.sh
+++ DataStandardization/DNA_Methylation.sh
@@ -7,7 +7,7 @@ then
 	rm DNA_MethylationFiles.sh
 else
 	wget "https://www.dropbox.com/s/8m7ourb5ucyvcf6/GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt"
-	mv 'GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt' GPL16304-47833.txt.gz
+	mv 'GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt' 'GPL16304-47833.txt.gz'
 	python3 CreateDNAmethDataCommands.py
     bash DNA_MethylationFiles.sh
     rm DNA_MethylationFiles.sh
