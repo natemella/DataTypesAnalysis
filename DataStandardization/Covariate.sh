711f8c6a468cf3a063a5c789697a80a58f74e316
nathanmella <nathanmell@gmail.com>
2019-05-24 17:42:14 -0600

    Reduced the Number of Cancer Types that I'll analyze and changed the output summary of Class.py

--- DataStandardization/Covariate.sh
+++ DataStandardization/Covariate.sh
@@ -5,14 +5,14 @@ extension=".CSV"
 if [ -e $fileName$extension ]
 then
     echo IT WORKED!!!
-    pip install expressionable-cli
+    pip3 install expressionable-cli
     expressionable $fileName $fileName$extension -i Excel -o CSV
     python3 Covariate.py $fileName
 #    cat $fileName
 else
     echo $fileName has not yest been downloaded
     wget https://www.cell.com/cms/10.1016/j.cell.2018.02.052/attachment/f4eb6b31-8957-4817-a41f-e46fd2a1d9c3/${fileName}
-    pip install expressionable-cli
+    pip3 install expressionable-cli
     expressionable $fileName $fileName$extension -i Excel -o CSV
     echo IT WORKED!!!
     python3 Covariate.py $fileName
