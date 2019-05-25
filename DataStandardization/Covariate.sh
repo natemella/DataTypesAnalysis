490a06e7045eef1c7ce39943abaf583f492bcdd4
nathanmella <nathanmell@gmail.com>
2019-05-24 18:34:19 -0600

    Reduced the Number of Cancer Types that I'll analyze and changed the output summary of Class.py

--- DataStandardization/Covariate.sh
+++ DataStandardization/Covariate.sh
@@ -4,19 +4,18 @@ fileName="mmc1.xlsx"
 extension=".CSV"
 if [ -e $fileName* ]
 then
-    echo IT WORKED!!!
-    mv $fileName mmc1.xls
-    mv mmc1.xls /src/xls/
-    docker run -v `pwd`xls:/src -it mrooding/docker-ssconvert-xls2csv
-    echo IT WORKED!!!
+#    echo IT WORKED!!!
+#    mv $fileName mmc1.xls
+#    mv mmc1.xls /src/xls/
+    docker run -v `pwd`/src/xls:/src -it mrooding/docker-ssconvert-xls2csv
 #    python3 Covariate.py $fileName
 #    cat $fileName
 else
-    echo $fileName has not yest been downloaded
-    wget https://www.cell.com/cms/10.1016/j.cell.2018.02.052/attachment/f4eb6b31-8957-4817-a41f-e46fd2a1d9c3/${fileName}
-    mv $fileName mmc1.xls
-    mv mmc1.xls /src/xls/
-    docker run -v `pwd`xls:/src -it mrooding/docker-ssconvert-xls2csv
+#    echo $fileName has not yest been downloaded
+#    wget https://www.cell.com/cms/10.1016/j.cell.2018.02.052/attachment/f4eb6b31-8957-4817-a41f-e46fd2a1d9c3/${fileName}
+#    mv $fileName mmc1.xls
+#    mv mmc1.xls /src/xls/
+    docker run -v `pwd`/src/xls:/src -it mrooding/docker-ssconvert-xls2csv
     echo IT WORKED!!!
 #    python3 Covariate.py $fileName
 fi
