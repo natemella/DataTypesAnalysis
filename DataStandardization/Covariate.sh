32b6a1998b4ea50b778f59d711a6e2de8a960658
nathanmella <nathanmell@gmail.com>
2019-05-24 18:09:15 -0600

    Reduced the Number of Cancer Types that I'll analyze and changed the output summary of Class.py

--- DataStandardization/Covariate.sh
+++ DataStandardization/Covariate.sh
@@ -2,60 +2,65 @@
 
 fileName="mmc1.xlsx"
 extension=".CSV"
-if [ -e $fileName$extension ]
+if [ -e $fileName* ]
 then
     echo IT WORKED!!!
-    pip3 install expressionable-cli
-    expressionable $fileName $fileName$extension -i Excel -o CSV
-    python3 Covariate.py $fileName
+    mkdir xls/
+    mv $fileName xls/
+    mkdir csv/
+    docker run -v `pwd`xls:/src -it mrooding/docker-ssconvert-xls2csv
+    echo IT WORKED!!!
+#    python3 Covariate.py $fileName
 #    cat $fileName
 else
     echo $fileName has not yest been downloaded
     wget https://www.cell.com/cms/10.1016/j.cell.2018.02.052/attachment/f4eb6b31-8957-4817-a41f-e46fd2a1d9c3/${fileName}
-    pip3 install expressionable-cli
-    expressionable $fileName $fileName$extension -i Excel -o CSV
+    mkdir xls/
+    mv $fileName xls/
+    mkdir csv/
+    docker run -v `pwd`xls:/src -it mrooding/docker-ssconvert-xls2csv
     echo IT WORKED!!!
-    python3 Covariate.py $fileName
+#    python3 Covariate.py $fileName
 fi
-rm $fileName
-rm $fileName$extension
-mv TCGA*.tsv ../
-cd ../
-if [ -d "InputData" ]
-then
-    mv TCGA*.tsv InputData/
-else
-    mkdir InputData
-    mv TCGA*.tsv InputData/
-fi
-
-cd InputData/
-
-Path=$(pwd)
-Files=${Path}/*
-
-for file in `ls `; do
-    IFS='.' read -ra cancertype <<< "$file"
-    mydir="${cancertype[0]}"
-    if [ -d $mydir"/Covariate/" ]
-    then
-        mv $mydir".tsv" $mydir/Covariate/
-    else
-        if [ -d $mydir ]
-        then
-            mkdir $mydir/Covariate
-            mv $mydir".tsv" $mydir/Covariate/
-        else
-            mkdir $mydir
-            mkdir $mydir/Covariate
-            mv $mydir".tsv" $mydir/Covariate/
-        fi
-    fi
-done
-
-cd ../
-cd DataStandardization/
-python3 Class.py
-rm summary.txt
-#rm result.tsv
-#rm cancer_test.csv
+#rm $fileName
+#rm $fileName$extension
+#mv TCGA*.tsv ../
+#cd ../
+#if [ -d "InputData" ]
+#then
+#    mv TCGA*.tsv InputData/
+#else
+#    mkdir InputData
+#    mv TCGA*.tsv InputData/
+#fi
+#
+#cd InputData/
+#
+#Path=$(pwd)
+#Files=${Path}/*
+#
+#for file in `ls `; do
+#    IFS='.' read -ra cancertype <<< "$file"
+#    mydir="${cancertype[0]}"
+#    if [ -d $mydir"/Covariate/" ]
+#    then
+#        mv $mydir".tsv" $mydir/Covariate/
+#    else
+#        if [ -d $mydir ]
+#        then
+#            mkdir $mydir/Covariate
+#            mv $mydir".tsv" $mydir/Covariate/
+#        else
+#            mkdir $mydir
+#            mkdir $mydir/Covariate
+#            mv $mydir".tsv" $mydir/Covariate/
+#        fi
+#    fi
+#done
+#
+#cd ../
+#cd DataStandardization/
+#python3 Class.py
+#rm summary.txt
+##rm result.tsv
+##rm cancer_test.csv
