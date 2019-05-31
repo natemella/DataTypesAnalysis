9f5ea9d1f2719b2da0c018324f92fc63517d82ed
nathanmella <nathanmell@gmail.com>
2019-05-31 13:44:02 -0600

    Keeping SARC with all molecular data types except Gene Expression

--- DataStandardization/DNA_Methylation.sh
+++ DataStandardization/DNA_Methylation.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-file="GPL16304-47833.txt.gz"
-if [ -e $file ] 
-then
-	python3 CreateDNAmethDataCommands.py
-	bash DNA_MethylationFiles.sh
-	rm DNA_MethylationFiles.sh
-else
-	wget "https://www.dropbox.com/s/8m7ourb5ucyvcf6/GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt"
-	mv 'GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt' $file
-#	 for c in `cat CancerTypes.txt`; do
-#	    echo "somefile_${c}.txt"; done;
-	python3 CreateDNAmethDataCommands.py
-    bash DNA_MethylationFiles.sh
-    rm DNA_MethylationFiles.sh
-fi
-rm $file
+#file="GPL16304-47833.txt.gz"
+#if [ -e $file ]
+#then
+#	python3 CreateDNAmethDataCommands.py
+#	bash DNA_MethylationFiles.sh
+#	rm DNA_MethylationFiles.sh
+#else
+#	wget "https://www.dropbox.com/s/8m7ourb5ucyvcf6/GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt"
+#	mv 'GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt' $file
+##	 for c in `cat CancerTypes.txt`; do
+##	    echo "somefile_${c}.txt"; done;
+#	python3 CreateDNAmethDataCommands.py
+#    bash DNA_MethylationFiles.sh
+#    rm DNA_MethylationFiles.sh
+#fi
+#rm $file
 
 cd output_Data
 mv *.* ../../
@@ -34,16 +34,16 @@ for file in `ls `; do
     mydir="${cancertype[0]}"
     if [ -d $mydir"/DNA_Methylation/" ]
     then
-        mv $mydir".tsv" $mydir/DNA_Methylation/
+        mv $file $mydir/DNA_Methylation/
     else
         if [ -d $mydir ]
         then
             mkdir $mydir/DNA_Methylation
-            mv $mydir".tsv" $mydir/DNA_Methylation/
+            mv $file $mydir/DNA_Methylation/
         else
             mkdir $mydir
             mkdir $mydir/DNA_Methylation
-            mv $mydir".tsv" $mydir/DNA_Methylation/
+            mv $file $mydir/DNA_Methylation/
         fi
     fi
 done
