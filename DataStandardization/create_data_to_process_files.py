d7c1db40e6fd93d511a9839a313ac5f29d6b88d5
nathanmella <nathanmell@gmail.com>
2019-06-03 17:12:29 -0600

    Made this work for more operating systems

--- DataStandardization/create_data_to_process_files.py
+++ DataStandardization/create_data_to_process_files.py
@@ -5,19 +5,37 @@ currentWorkingDir = os.path.dirname(os.path.realpath(__file__))
 
 RelevantTypes = ()
 
+def path_to_list(path):
+  folders = []
+  while True:
+    path, folder = os.path.split(path)
+    if folder:
+      folders.append(folder)
+    else:
+      if path:
+        folders.append(path)
+      break
+  folders.reverse()
+  return folders
+
+def sep_maker():
+  list = ['a','b']
+  x = os.path.join(*list)
+  return x[1]
+
 
 def checkfunction(dtype):
     # write_dir(output_directory, INPUT_DATA, "PFI", list_of_dTypes, os.listdir(d_type_directory), Covariate_dir)
-    if not os.path.exists(os.path.dirname(f'{output_directory}/{dtype}.txt')):
-        File1 = open(f'{output_directory}/{dtype}.txt', 'w+')
+    if not os.path.exists(os.path.dirname(f'{output_directory}{_}{dtype}.txt')):
+        File1 = open(f'{output_directory}{_}{dtype}.txt', 'w+')
     else:
-        File1 = open(f'{output_directory}/{dtype}.txt', 'a')
+        File1 = open(f'{output_directory}{_}{dtype}.txt', 'a')
 
     if dtype != "Covariate":
-        if not os.path.exists(os.path.dirname(f'{output_directory}/{dtype}_and_Covariate.txt')):
-            File2 = open(f'{output_directory}/{dtype}_and_Covariate.txt', 'w+')
+        if not os.path.exists(os.path.dirname(f'{output_directory}{_}{dtype}_and_Covariate.txt')):
+            File2 = open(f'{output_directory}{_}{dtype}_and_Covariate.txt', 'w+')
         else:
-            File2 = open(f'{output_directory}/{dtype}_and_Covariate.txt', 'a')
+            File2 = open(f'{output_directory}{_}{dtype}_and_Covariate.txt', 'a')
         return [File1, File2]
     else:
         return [File1]
@@ -34,23 +52,27 @@ def pop_back(file):
 endpoints = sys.argv[1:]
 
 
-my_list = currentWorkingDir.split('/')
-parent_directory = '/'.join(my_list[:-1])
-output_directory = f'{parent_directory}/Data_To_Process_Files/'
+my_list = path_to_list(currentWorkingDir)
+parent_directory = sep_maker().join(my_list[:-1])
+output_directory = os.path.join(*[parent_directory,f'Data_To_Process_Files{sep_maker()}'])
 if not os.path.exists(os.path.dirname(output_directory)):
     os.makedirs(os.path.dirname(output_directory))
 else:
     shutil.rmtree(os.path.dirname(output_directory))
     os.makedirs(os.path.dirname(output_directory))
 
-INPUT_DATA = next(os.walk(parent_directory + "/InputData"))[1]
+INPUT_DATA = next(os.walk(parent_directory + f"{sep_maker()}InputData"))[1]
 
-for CancerType in next(os.walk(parent_directory + "/InputData"))[1]:
-    for list_of_dTypes in next(os.walk(parent_directory + "/InputData/" + CancerType)):
+# for unix it would be '{_}' for windows it would be '\'
+_=sep_maker()
+
+
+for CancerType in INPUT_DATA:
+    for list_of_dTypes in next(os.walk(os.path.join(*[parent_directory,"InputData",CancerType]))):
         if len(list_of_dTypes) > 1 and isinstance(list_of_dTypes, list):
             for DataType in list_of_dTypes:
-                d_type_directory = f"{parent_directory}/InputData/{CancerType}/{DataType}"
-                Covariate_dir = f"{parent_directory}/InputData/{CancerType}/Covariate/"
+                d_type_directory = f"{parent_directory}{_}InputData{_}{CancerType}{_}{DataType}"
+                Covariate_dir = f"{parent_directory}{_}InputData{_}{CancerType}{_}Covariate{_}"
                 if DataType != "Class":
                     myFiles = checkfunction(DataType)
                     if DataType == "Covariate":
