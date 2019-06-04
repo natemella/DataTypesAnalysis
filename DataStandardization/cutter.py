654554cd608aebcc2c96e41785b3fa1fffc2de96
nathanmella <nathanmell@gmail.com>
2019-06-04 12:50:51 -0600

    Lowered Analysis to only 12 Cancer Types

--- DataStandardization/cutter.py
+++ DataStandardization/cutter.py
@@ -50,13 +50,14 @@ for CancerType in INPUT_DATA:
     patients_with_all = set()
     patients_per_data = []
     for list_of_dTypes in next(os.walk(os.path.join(*[parent_directory,"InputData",CancerType]))):
+        already_seen = False
         if len(list_of_dTypes) > 1 and isinstance(list_of_dTypes, list):
             for DataType in list_of_dTypes:
                 d_type_directory = f"{parent_directory}{_}InputData{_}{CancerType}{_}{DataType}"
-                if DataType == "Covariate":
-                    continue
                 if DataType != "Class":
                     for input_file in os.listdir(d_type_directory):
+                        if already_seen:
+                            continue
                         input_file = f'{d_type_directory}{_}{input_file}'
                         if input_file.endswith(('.tsv','.txt')):
                             patients_per_data = [line.split('\t')[0] for line in open(input_file)]
@@ -74,7 +75,10 @@ for CancerType in INPUT_DATA:
                                 sample_summary.write(f'{DataType}:{len(patients_per_data)}|')
                                 total_patients.update(patients_per_data)
                                 patients_with_all.intersection((set(patients_per_data)))
+                        if DataType == "Covariate":
+                            already_seen = True
                 else:
                     patients_with_all.update(line.split('\t')[0] for line in open(f'{d_type_directory}{_}PFI.txt'))
+                    sample_summary.write(f'{DataType}:{len(patients_with_all)}|')
     sample_summary.write(f'\t{len(total_patients)}\t{len(patients_with_all)}\n')
 sample_summary.close()
