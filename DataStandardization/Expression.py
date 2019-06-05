3213326c2adf4b791d5afe5ec54f9c47098143d8
nathanmella <nathanmell@gmail.com>
2019-06-05 14:26:53 -0600

    I fixed an erro in util.py

--- DataStandardization/Expression.py
+++ DataStandardization/Expression.py
@@ -1,63 +1,20 @@
 import pandas as pd
 import sys
-
-with open("CancerTypes.txt") as file:
-    with open("abreviations.tsv") as abr:
-        abreviations = [x.strip('\n') for x in file]
-        abreviations = set(abreviations)
-        print(abreviations)
-        abreviations.remove("SARC")
-        RelevantTypes = [x.split('\t')[1].strip('\n') for x in abr if x.split('\t')[0] in abreviations]
-
-CancerDict = {}
-for x in RelevantTypes:
-    CancerDict[x] = []
-
-TSSDictionary = {}
-
-RelevantCodes = set()
-
-#include the index column in each list
-
-with open("TSS_CODES.tsv") as codes:
-    first_line = codes.readline()
-    for x in codes:
-        line = x.strip('\n').split('\t')
-        if line[2] in RelevantTypes:
-            RelevantCodes.add(line[0])
-            TSSDictionary[line[0]] = line[2]
-
-# So that the outputfile will have the correct names
-Abbreviations_Dict = {}
-with open("abreviations.tsv") as abr:
-    first_line = abr.readline()
-    for x in abr:
-        list = x.strip('\n').split('\t')
-        if list[1] in RelevantTypes:
-            Abbreviations_Dict[list[1]] = list[0]
-
-
-file_name = sys.argv[1]
-CancerPatientIDs = []
+from util import *
 
 with open(sys.argv[1]) as input:
-    AllPatients= [x for x in input.readline().strip('\n').split('\t') if x != '']
-    CancerIndex = 0
-    for x in AllPatients:
-        if x.split('-')[1] in RelevantCodes:
-            CancerPatientIDs.append(x)
-            tss = x.split('-')[1]
-            for Cancer in RelevantTypes:
-                if TSSDictionary[tss] == Cancer:
-                    CancerDict[Cancer].append(x)
+    all_patients= [x for x in input.readline().strip('\n').split('\t') if x != '']
+    list_of_dictionaries = dictionary_makers(all_patients)
     index_col = [line.split('\t')[0] for line in input]
+
+abbreviations_dict = list_of_dictionaries[3]
+cancer_dict = list_of_dictionaries[4]
+
 print("Finished Step 1")
 
-# df = pd.read_csv(sys.argv[1], delimiter='\t', usecols=CancerPatientIDs)[CancerPatientIDs]
-# df = df.set_index("sample")
-i = 0
-for x in CancerDict:
-    df = pd.read_csv(sys.argv[1], delimiter='\t', usecols=CancerDict[x], engine='c', na_values='NA')[CancerDict[x]].astype(float)
+i = 1
+for x in cancer_dict:
+    df = pd.read_csv(sys.argv[1], delimiter='\t', usecols=cancer_dict[x], engine='c', na_values='NA')[cancer_dict[x]].astype(float)
     s = pd.Series(index_col)
     s.name = "SampleID"
     df = pd.concat([s,df], axis=1)
@@ -77,5 +34,5 @@ for x in CancerDict:
         new_columns.append(truncatedID)
     df.columns = new_columns
     print(f'Beginning to write DataFrame to file')
-    df.to_csv(path_or_buf=('TCGA_' + Abbreviations_Dict[x] + '.ttsv'), sep='\t', na_rep="NA", index=False)
-    print(f'Finished Evaluating TCGA_{Abbreviations_Dict[x]}')
+    df.to_csv(path_or_buf=('TCGA_' + abbreviations_dict[x] + '.ttsv'), sep='\t', na_rep="NA", index=False)
+    print(f'Finished Evaluating TCGA_{abbreviations_dict[x]}')
