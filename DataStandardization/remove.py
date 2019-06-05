9ee8317e7a9762fcad605d9a5cbde60eed747597
nathanmella <nathanmell@gmail.com>
2019-06-05 14:02:07 -0600

    Cleaned up Class.py and Covariate.py

--- DataStandardization/remove.py
+++ DataStandardization/remove.py
@@ -1,47 +1,20 @@
 import os
 import shutil
-currentWorkingDir = os.path.dirname(os.path.realpath(__file__))
+from util import *
 
+current_working_dir = os.path.dirname(os.path.realpath(__file__))
+relevant_types = make_relevant_types_list()
+abbreviations_dict = make_abbrevation_dict(relevant_types)
+folders_list = path_to_list(current_working_dir)
+parent_directory = path_delimiter().join(folders_list[:-1])
 
-with open("CancerTypes.txt") as file:
-    with open("abreviations.tsv") as abr:
-        abreviations = [x.strip('\n') for x in file]
-        RelevantTypes = [x.split('\t')[1].strip('\n') for x in abr if x.split('\t')[0] in abreviations]
+_= path_delimiter()
 
-# So that the outputfile will have the correct names
-Abbreviations_Dict = {}
-with open("abreviations.tsv") as abr:
-    first_line = abr.readline()
-    for x in abr:
-        list = x.strip('\n').split('\t')
-        if list[1] in RelevantTypes:
-            Abbreviations_Dict["TCGA_" + list[0]] = list[1]
+# need to invert dictionary
 
-def path_to_list(path):
-  folders = []
-  while True:
-    path, folder = os.path.split(path)
-    if folder:
-      folders.append(folder)
-    else:
-      if path:
-        folders.append(path)
-      break
-  folders.reverse()
-  return folders
+abbreviations_dict = {value: key for key, value in abbreviations_dict.items()} # reverse keys and values
 
-def sep_maker():
-  list = ['a','b']
-  x = os.path.join(*list)
-  return x[1]
-
-list = path_to_list(currentWorkingDir)
-parent_directory = sep_maker().join(list[:-1])
-
-# for CancerType, DataType, File in os.walk(parent_directory + '/InputData'):
-#     print(CancerType)
-_=sep_maker()
 for x in next(os.walk(f'{parent_directory}{_}InputData'))[1]:
-    if x not in Abbreviations_Dict:
+    if x.split('_')[1] not in abbreviations_dict:
         shutil.rmtree(f'{parent_directory}{_}InputData{_}{x}')
         print(x)
--- DataStandardization/util.py
+++ DataStandardization/util.py
@@ -20,20 +20,21 @@ def path_delimiter():
     x = os.path.join(*Folders)
     return x[1]
 
-
-def dictionary_makers(all_patients):
+def make_relevant_types_list():
     with open("CancerTypes.txt") as file:
         with open("abreviations.tsv") as abr:
             abreviations = [x.strip('\n') for x in file]
             relevant_types = [x.split('\t')[1].strip('\n') for x in abr if x.split('\t')[0] in abreviations]
+    return relevant_types
 
+def make_cancer_dict(relevant_types):
     cancer_dict = {}
     for x in relevant_types:
         cancer_dict[x] = []
+    return cancer_dict
 
+def make_tss_dict_and_rev_codes_dic(relevant_types):
     relevant_codes = set()
-
-
     tss_dictionary = {}
     with open("TSS_CODES.tsv") as codes:
         codes.readline()
@@ -42,8 +43,9 @@ def dictionary_makers(all_patients):
             if line[2] in relevant_types:
                 relevant_codes.add(line[0])
                 tss_dictionary[line[0]] = line[2]
+    return [relevant_codes, tss_dictionary]
 
-    # So that the outputfile will have the correct names
+def make_abbrevation_dict(relevant_types):
     abbreviations_dict = {}
     with open("abreviations.tsv") as abr:
         abr.readline()
@@ -51,7 +53,9 @@ def dictionary_makers(all_patients):
             list = x.strip('\n').split('\t')
             if list[1] in relevant_types:
                 abbreviations_dict[list[1]] = list[0]
+    return abbreviations_dict
 
+def fill_cancer_dict(relevant_codes, tss_dictionary, cancer_dict, relevant_types, all_patients):
     cancer_patient_ids = []
 
     for sample_id in all_patients:
@@ -61,5 +65,14 @@ def dictionary_makers(all_patients):
             for Cancer in relevant_types:
                 if tss_dictionary[tss] == Cancer:
                     cancer_dict[Cancer].append(sample_id)
+
+def dictionary_makers(all_patients):
+
+    relevant_types = make_relevant_types_list()
+    cancer_dict = make_cancer_dict(relevant_types)
+    relevant_codes = make_tss_dict_and_rev_codes_dic(relevant_types)[0]
+    tss_dictionary = make_tss_dict_and_rev_codes_dic(relevant_types)[1]
+    abbreviations_dict = make_abbrevation_dict(relevant_types)
+    cancer_patient_ids = (relevant_codes, tss_dictionary, cancer_dict, relevant_types, all_patients)
 
     return [relevant_types, relevant_codes, tss_dictionary, abbreviations_dict, cancer_dict, cancer_patient_ids]
