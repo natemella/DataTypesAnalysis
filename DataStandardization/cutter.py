16c923296b5b3da6db2c1ab645840674ea4c2f3f
nathanmella <nathanmell@gmail.com>
2019-06-07 15:59:47 -0600

    Cleaning up bash scripts so that they are more readible

--- DataStandardization/cutter.py
+++ DataStandardization/cutter.py
@@ -1,19 +1,39 @@
 import os
+import pandas as pd
 currentWorkingDir = os.path.dirname(os.path.realpath(__file__))
 import codecs
 from util import *
 
-RelevantTypes = ()
+def filter_cols(input_file, patient_ids):
+    df = pd.read_csv(input_file, delimiter='\t',
+                       na_values='NA')[patient_ids]
+    print(df.columns.values[0:10])
+    return df
 
+def filter_rows(input_file, patient_ids, index_name):
+    iter_csv = pd.read_csv(input_file, delimiter='\t', iterator=True, chunksize=1000)
+    return pd.concat([chunk[chunk[index_name].isin(patient_ids)] for chunk in iter_csv])
+def make_df(patient_ids, DataType, input_file):
+    print(DataType)
 
+    if input_file.endswith(".ttsv"):
+        df = filter_cols(input_file, patient_ids)
+    else:
+        if DataType != "DNA_methylation":
+            index_name = "SampleID"
+        else:
+            index_name = "Patient_ID"
+        df = filter_rows(input_file,patient_ids, index_name)
+
+    print(df.iloc[:3])
 
 my_list = path_to_list(currentWorkingDir)
 parent_directory = path_delimiter().join(my_list[:-1])
 
-INPUT_DATA = next(os.walk(parent_directory + f"{path_delimiter()}InputData"))[1]
+INPUT_DATA = next(os.walk(os.path.join(*[parent_directory,"InputData"])))[1]
 
 # for unix it would be '{_}' for windows it would be '\'
-delimiter=path_delimiter()
+_=path_delimiter()
 
 sample_summary = open("sample_summary.tsv",'w+')
 sample_summary.write("CancerType\tOutcome\tNumber of Patients per type of Data\t"
@@ -25,7 +45,7 @@ end_points = ["LT_PFI", "ST_PFI"]
 for CancerType in INPUT_DATA:
     for outcome in end_points:
         sample_summary.write(f'{CancerType}\t')
-        sample_summary.write(f'{outcome.replace("T_","")}\t')
+        sample_summary.write(f'{outcome.replace("T_", "")}\t')
         total_patients = set()
         patients_with_all = set()
         patients_per_data = []
@@ -33,34 +53,30 @@ for CancerType in INPUT_DATA:
             already_seen = False
             if len(list_of_dTypes) > 1 and isinstance(list_of_dTypes, list):
                 for DataType in list_of_dTypes:
-                    d_type_directory = f"{parent_directory}{delimiter}InputData{delimiter}{CancerType}{delimiter}{DataType}"
+                    d_type_directory = os.path.join(*[parent_directory,"InputData",CancerType,DataType])
                     if DataType != "Class":
-                        if DataType != "Expression":
-                            continue
                         for input_file in os.listdir(d_type_directory):
                             if already_seen and DataType == "Covariate":
                                 continue
-                            input_file = f'{d_type_directory}{delimiter}{input_file}'
+                            input_file = f'{d_type_directory}{_}{input_file}'
                             if input_file.endswith(('.tsv','.txt')):
                                 patients_per_data = [line.split('\t')[0] for line in open(input_file)]
-                                patients_per_data.pop(0)
                                 total_patients.update(patients_per_data)
-                                patients_with_all.intersection(set(patients_per_data))
-                                sample_summary.write(f'{DataType}:overlaping = {len(patients_with_all)}, total_samples = {len(patients_per_data)}|')
+                                patients_with_all = patients_with_all.intersection(set(patients_per_data))
+                                sample_summary.write(f'{DataType}:overlaping = {len(patients_with_all)},'
+                                                     f' total_samples = {len(patients_per_data)}')
                             else:
                                 with codecs.open(input_file, 'r') as myfile:
                                     firstline = myfile.readline()
                                     patients_per_data = firstline.split('\t')
-                                    patients_per_data.pop(0)
                                     total_patients.update(patients_per_data)
-                                    patients_with_all.intersection(set(patients_per_data))
-                                    sample_summary.write(f'{DataType}:overlaping = {len(patients_with_all)}, total_samples = {len(patients_per_data)}|')
+                                    patients_with_all = patients_with_all.intersection(set(patients_per_data))
+                                    sample_summary.write(f'{DataType}:overlaping = {len(patients_with_all)},'
+                                                         f' total_samples = {len(patients_per_data)}')
                             if DataType == "Covariate":
                                 already_seen = True
                     else:
-                        patients_per_data = [line.split('\t')[0] for line in open(f'{d_type_directory}{delimiter}PFI.txt') if line.strip('\n').split('\t')[1] == outcome]
-                        patients_per_data.pop(0)
+                        patients_per_data = [line.split('\t')[0] for line in open(f'{d_type_directory}{_}PFI.txt') if line.strip('\n').split('\t')[1] == outcome]
                         patients_with_all.update(patients_per_data)
-                        sample_summary.write(f'{DataType}:{len(patients_with_all)}|')
-        sample_summary.write(f'\t{len(total_patients)}\t{len(patients_with_all)}\n')
+
 sample_summary.close()
