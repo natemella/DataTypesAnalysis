f5c6b464fb6b226eecfb01f82a6b47f739b6ff2c
nathanmella <nathanmell@gmail.com>
2019-06-08 00:24:23 -0600

    Working on cutter.py for short analysis

--- DataStandardization/cutter.py
+++ DataStandardization/cutter.py
@@ -3,31 +3,56 @@ import pandas as pd
 currentWorkingDir = os.path.dirname(os.path.realpath(__file__))
 import codecs
 from util import *
-
-def run_make_df_function(d_type_directory, patients_with_all, DataType):
+import argparse
+parser = argparse.ArgumentParser(description="Develop a summary of file information and cut the file.")
+parser.add_argument(
+    "-c",
+    "--cut-files",
+    type=str,
+    default="False",
+    help=("bool on whether to shorten files of a data type so that they only contain the SampleID's"
+         " found in the class files."
+          )
+)
+parser.add_argument(
+    "-m",
+    '--mini',
+    type=str,
+    default="False",
+    help="Shorten files to 3 variables for mini, quick analysis."
+)
+args = parser.parse_args()
+cut_files = args.cut_files
+mini_analysis = args.mini
+print(f"Cut files set to {cut_files}")
+print(f"mini_analysis set to {mini_analysis}")
+def run_make_df_function(d_type_directory, patients_with_all, DataType, mini):
         if DataType == "Class":
             return
         for input_file in os.listdir(d_type_directory):
             input_file = f'{d_type_directory}{_}{input_file}'
-            make_df(patients_with_all, DataType, input_file)
+            make_df(patients_with_all, DataType, input_file, mini)
 
-def filter_cols(input_file, patient_ids):
+def filter_cols(input_file, patient_ids, mini):
     my_cols = list(patient_ids)
     my_cols.insert(0,"SampleID")
     df = pd.read_csv(input_file, delimiter='\t',
                        na_values='NA', usecols=my_cols, index_col="SampleID")
-    print(df.iloc[:3])
+    if mini == "True":
+        df = df.iloc[:3]
     return df
 
-def filter_rows(input_file, patient_ids, index_name):
+def filter_rows(input_file, patient_ids, index_name, mini):
     iter_csv = pd.read_csv(input_file, delimiter='\t', iterator=True, chunksize=1000)
     df = pd.concat([chunk[chunk[index_name].isin(patient_ids)] for chunk in iter_csv])
     df = df.set_index(index_name)
-    print(df.iloc[:,0:3])
-def make_df(patient_ids, DataType, input_file):
+    if mini == "True":
+        df = df.iloc[:,0:3]
+    return df
+def make_df(patient_ids, DataType, input_file, mini):
     print(DataType)
     if input_file.endswith(".ttsv"):
-        df = filter_cols(input_file, patient_ids)
+        df = filter_cols(input_file, patient_ids, mini)
     else:
         if DataType != "DNA_methylation":
             index_name = "SampleID"
@@ -35,8 +60,10 @@ def make_df(patient_ids, DataType, input_file):
             index_name = "Patient_ID"
         if DataType == "Covariate" and input_file.endswith(".tsv"):
             return
-        df = filter_rows(input_file,patient_ids, index_name)
-
+        df = filter_rows(input_file,patient_ids, index_name, mini)
+    print(f"Rewriting {path_to_list(input_file)[-1]}")
+    print(df)
+    # df.to_csv(path_or_buf=input_file, sep='\t')
 
 my_list = path_to_list(currentWorkingDir)
 parent_directory = path_delimiter().join(my_list[:-1])
@@ -94,7 +121,8 @@ for CancerType in input_data_dir:
                         class_info.update(patients_per_data)
                         sample_summary.write(f'{DataType}:{len(patients_with_all)},')
                     sample_summary.write(' | ')
-                    # run_make_df_function(d_type_directory,patients_with_all,DataType)
+                    if cut_files == "True":
+                        run_make_df_function(d_type_directory,patients_with_all,DataType,mini_analysis)
                 sample_summary.write(f',{len(patients_with_all)}\n')
     sample_summary.write('\n')
 
