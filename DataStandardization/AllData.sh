3440b6f0ef34b05cbb82d09177389ba2147d1fa5
nathanmella <nathanmell@gmail.com>
2019-06-11 13:47:53 -0600

    messing with get_metrics

--- DataStandardization/AllData.sh
+++ DataStandardization/AllData.sh
@@ -15,7 +15,7 @@ echo -------------------
 bash CNV.sh
 echo -------------------
 echo Finished CNV
-echo --------------------
+echo -------------------
 bash SM.sh
 echo -------------------
 echo Finished SM
@@ -24,3 +24,6 @@ bash Expression.sh
 echo -------------------
 echo Finished Expression
 echo -------------------
+bash DNA_Methylation.sh
+echo -------------------
+echo Finished DNA Methylation
