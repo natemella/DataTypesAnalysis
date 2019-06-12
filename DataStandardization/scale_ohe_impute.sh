a6a6a903d0098fb8aa90535adf1822803669d319
nathanmella <nathanmell@gmail.com>
2019-06-12 10:33:32 -0600

    messing with get_metrics

--- DataStandardization/scale_ohe_impute.sh
+++ DataStandardization/scale_ohe_impute.sh
@@ -37,13 +37,13 @@ echo $extension
 }
 
 cd ..
-#for c in `python3 DataStandardization/get_paths.py`; do
-#    gunzip_if_gzipped InputData$c
-#done
-#for c in `python3 DataStandardization/get_paths.py`; do
-#    echo gzipping $c
-#    gzip InputData$c
-#done
+for c in `python3 DataStandardization/get_paths.py`; do
+    gunzip_if_gzipped InputData$c
+done
+for c in `python3 DataStandardization/get_paths.py`; do
+    echo gzipping $c
+    gzip InputData$c
+done
 for c in `python3 DataStandardization/get_paths.py`; do
     echo imputing $c
     imputing $c
