cd284d3e3964c327582f95b14f074a0638dd706c
nathanmella <nathanmell@gmail.com>
2019-06-21 11:11:51 -0600

    Setting up my code so that it can be run with docker

---
+++ DataStandardization/Clinical.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+. ./functions.sh
+fileName="GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz"
+web_url="ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE62nnn/GSE62944/suppl/"
+python_script=Clinical.py
+folder="Clinical"
+tcga_extension=".tsv"
+
+download_and_organize_data $fileName $python_script $web_url $tcga_extension $folder
+
+
+
