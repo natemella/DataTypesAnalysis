59daaa85efa3faa8a04203304d36812b70463ce4
nathanmella <nathanmell@gmail.com>
2019-06-21 20:27:51 -0600

    I changed how clinical data handles categorical data

--- DataStandardization/Clinical.py
+++ DataStandardization/Clinical.py
@@ -29,6 +29,14 @@ def fix_tumor_grade(df):
     df.tumor_grade = [int(data_point[1]) if not pd.isna(data_point) else data_point for data_point in df.tumor_grade]
     return df
 
+def adjust_race_labels(df):
+    df.race = ["OTHER" if data in ["ASIAN","NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER","AMERICAN INDIAN OR ALASKA NATIVE"] else data for data in df.race]
+    return df
+
+def combine_yes_values_history_other_malignancy(df):
+    df.history_other_malignancy = ["Yes" if not pd.isna(data_point) and "Yes" in data_point else data_point for data_point in df.history_other_malignancy]
+    return df
+
 def fix_tumor_stage_labels(df, cancer_type):
     series = df.ajcc_pathologic_tumor_stage
     new_column = []
@@ -153,6 +161,13 @@ def check_num_of_patients_per_category(df):
                 categorical_columns[i].append(f'{x}:{my_map[x]}')
     with open("excluded_categories.txt", 'a') as out:
         for i in categorical_columns:
+            if len(categorical_columns[i]) == 1:
+                variables_to_drop.append(i)
+                print(f'\n{i}')
+                out.write(f'\n{i}\n')
+                print(f'{categorical_columns[i]}\n')
+                out.write(f'{categorical_columns[i]}\n\n')
+                continue
             for item in categorical_columns[i]:
                 item = item.split(":")
                 if int(item[-1]) < 5:
@@ -166,7 +181,19 @@ def check_num_of_patients_per_category(df):
     # df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in categorical_columns.items() ]))
     # df.to_csv(path_or_buf=("category_info.tsv"), sep='\t')
 
-
+def filter_extrathyroidal_extension(df):
+    adjusted_data = []
+    for value in df.extrathyroidal_extension:
+        if value == "None":
+            adjusted_data.append(0)
+        elif value == "Minimal (T3)":
+            adjusted_data.append(3)
+        elif value == "Moderate/Advanced (T4a)" or "Very Advanced (T4b)":
+            adjusted_data.append(4)
+        else:
+            adjusted_data.append(value)
+    df.extrathyroidal_extension = adjusted_data
+    return df
 
 def find_all_unique_values(series, name):
     unique_values = set()
@@ -189,6 +216,37 @@ def filter_history_thyroid_disease(df):
     new_df= find_all_unique_values(column, "history_thyroid_disease")
     df = df.drop("history_thyroid_disease", axis=1)
     df = pd.concat([df, new_df], axis=1)
+    return df
+
+
+def combine_small_categories(df, variable):
+    my_set = set(df[variable])
+    my_map = {}
+    for category in my_set:
+        my_map[category] = len([data for data in df[variable] if data == category])
+    df[variable] = ["Other" if my_map[data_point] < 10 else data_point for data_point in df[variable]]
+    return df
+
+def combine_smoking_history(df):
+    df.tobacco_smoking_history_indicator = ["Current reformed smoker for < or = 15 years" if data == "Current Reformed Smoker, Duration Not Specified" else data for data in df.tobacco_smoking_history_indicator]
+    return df
+
+def change_laterality_to_binary(df):
+    right_side = [1 if data == "Right" or data == "Bilateral" else 0 for data in df.laterality]
+    left_side = [1 if data == "Left" or data == "Bilateral" else 0 for data in df.laterality]
+    new_df = pd.DataFrame({'laterality_right':right_side,'laterality_left':left_side})
+    df = df.drop("laterality", axis=1)
+    df = pd.concat([df,new_df],axis=1)
+    return df
+
+def combine_anatomic_subdivision(df):
+    new_col = []
+    for data in df.anatomic_organ_subdivision:
+        if data == "Other (please specify)" or data == "Bronchial":
+            new_col.append("Other")
+        else:
+            new_col.append(data)
+    df.anatomic_organ_subdivision = new_col
     return df
 
 def drop_non_useful_variables(df):
@@ -256,6 +314,9 @@ for sample_id in cancer_dict:
     new_index = [patient_id[0:12] for patient_id in my_index]
     one_cancer_df.index = new_index
     one_cancer_df = check_for_duplicates_categorical(one_cancer_df)
+
+    if "race" in one_cancer_df.columns.values:
+        one_cancer_df = adjust_race_labels(one_cancer_df)
     if "tumor_grade" in one_cancer_df.columns.values:
         one_cancer_df = fix_tumor_grade(one_cancer_df)
     if "residual_tumor" in one_cancer_df.columns.values:
@@ -272,14 +333,38 @@ for sample_id in cancer_dict:
         one_cancer_df = fix_tumor_stage_labels(one_cancer_df, abbreviations_dict[sample_id])
     if "clinical_stage" in one_cancer_df.columns.values:
         one_cancer_df = fix_clinical_stage_labels(one_cancer_df)
+
     if "clinical_T" in one_cancer_df.columns.values:
         one_cancer_df = fix_clinical_T_lables(one_cancer_df)
     if "clinical_M" in one_cancer_df.columns.values:
         one_cancer_df = fix_clinical_M_lables(one_cancer_df)
     if "clinical_N" in one_cancer_df.columns.values:
         one_cancer_df = fix_clinical_N_lables(one_cancer_df)
+    if "extrathyroidal_extension" in one_cancer_df.columns.values:
+        one_cancer_df =filter_extrathyroidal_extension(one_cancer_df)
+
     if abbreviations_dict[sample_id] == "BLCA":
         one_cancer_df = one_cancer_df.drop(["history_neoadjuvant_treatment","ethnicity"], axis="columns")
+    if abbreviations_dict[sample_id] in ["LGG","KIRC","PRAD"]:
+        one_cancer_df = combine_yes_values_history_other_malignancy(one_cancer_df)
+    if abbreviations_dict[sample_id] == "LGG":
+        one_cancer_df = one_cancer_df.drop(["history_neoadjuvant_treatment","history_ionizing_rt_to_head"], axis="columns")
+        one_cancer_df = combine_small_categories(one_cancer_df, "tumor_site")
+    if abbreviations_dict[sample_id] == "HNSC":
+        one_cancer_df = one_cancer_df.drop("histologic_diagnosis", axis="columns")
+        one_cancer_df = combine_small_categories(one_cancer_df, "anatomic_organ_subdivision")
+    if abbreviations_dict[sample_id] == "HNSC" or "LUAD":
+        if "tobacco_smoking_history_indicator" in one_cancer_df.columns.values:
+            one_cancer_df = combine_smoking_history(one_cancer_df)
+    if abbreviations_dict[sample_id] == "KIRC":
+        one_cancer_df = change_laterality_to_binary(one_cancer_df)
+    if abbreviations_dict[sample_id] == "LUAD":
+        one_cancer_df = one_cancer_df.drop("history_neoadjuvant_treatment", axis=1)
+        one_cancer_df = combine_anatomic_subdivision(one_cancer_df)
+        one_cancer_df = combine_small_categories(one_cancer_df, "histologic_diagnosis.1")
+    if abbreviations_dict[sample_id] == "UCEC":
+        one_cancer_df = one_cancer_df.drop("history_neoadjuvant_treatment", axis=1)
+
     one_cancer_df = check_num_of_patients_per_category(one_cancer_df)
     one_cancer_df.to_csv(path_or_buf=('TCGA_' + abbreviations_dict[sample_id] + '.tsv'), sep='\t', na_rep='NA')
 
