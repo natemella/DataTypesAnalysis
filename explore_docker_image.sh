4c0ea84a03186525341effcc9a2e2cdc88254500
nathanmella <nathanmell@gmail.com>
2019-06-25 13:59:19 -0600

    Dockerized My Repository

--- explore_docker_image.sh
+++ explore_docker_image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker run --rm -it --entrypoint=/bin/bash \
-       -v "InputData/":"DataTypesAnalysis/InputData" \
-       -v "Analysis_Results":"DataTypesAnalysis/Analysis_Results" dta
+       -v "$(pwd)/InputData/":"DataTypesAnalysis/InputData" \
+       -v "$(pwd)/Analysis_Results":"DataTypesAnalysis/Analysis_Results" dta
