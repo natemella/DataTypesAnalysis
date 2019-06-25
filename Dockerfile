394d8fcc4d4eb48e023f9d0091e12313dccf3a80
nathanmella <nathanmell@gmail.com>
2019-06-25 14:21:55 -0600

    Dockerized My Repository

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ RUN pip install --no-cache-dir -r requirements.txt \
     && git clone https://github.com/natemella/DataTypesAnalysis.git \
     && mkdir DataTypesAnalysis/InputData \
     && mkdir DataTypesAnalysis/Analysis_Results \
-    && chmod -R 755
+    && mkdir DataTypesAnalysis/Permanent_Results \
+    && find DataTypesAnalysis/ -type d -exec chmod 755 {} \;
 
 #COPY InputData/ DataTypesAnalysis/InputData
 
--- explore_docker_image.sh
+++ explore_docker_image.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+if [ ! -d "InputData/" ]; then
+mkdir InputData/
+fi
+
+if [ ! -d "Analysis_Results/" ]; then
+mkdir Analysis_Results/
+fi
+
+if [ ! -d "Permanent_Results/" ]; then
+mkdir Permanent_Results/
+fi
+
 docker run --rm -it --entrypoint=/bin/bash \
        -v "$(pwd)/InputData/":"/DataTypesAnalysis/InputData/" \
-       -v "$(pwd)/Analysis_Results/":"/DataTypesAnalysis/Analysis_Results/" dta
+       -v "$(pwd)/Analysis_Results/":"/DataTypesAnalysis/Analysis_Results/" \
+       -v "$(pwd)/Permanent_Results/":"/DataTypesAnalysis/Permanent_Results/" dta
