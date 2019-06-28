797dc402d9e42f3863a7e0ca8d6b92ac7ca584bc
nathanmella <nathanmell@gmail.com>
2019-06-28 13:13:54 -0600

    Modified run_docker to fix error

--- DataStandardization/DNA_Methylation.sh
+++ DataStandardization/DNA_Methylation.sh
@@ -3,9 +3,9 @@ file="GPL16304-47833.txt.gz"
 set -e
 if [ -e $file ]
 then
-	python3 CreateDNAmethDataCommands.py
-	bash DNA_MethylationFiles.sh
-	rm DNA_MethylationFiles.sh
+	for c in `cat CancerTypes.txt`; do
+	    DNA_Methylation.R "https://tcga.xenahubs.net/download/TCGA."$c".sampleMap/HumanMethylation450.gz" "TCGA_"$c".tsv";
+	done;
 else
 	wget "https://www.dropbox.com/s/8m7ourb5ucyvcf6/GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt"
 	mv 'GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt' $file
