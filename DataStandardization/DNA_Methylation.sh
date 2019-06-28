6337e118bf6e4a00c6f79a1325daf65bb564524e
nathanmella <nathanmell@gmail.com>
2019-06-28 13:14:48 -0600

    Modified run_docker to fix error

--- DataStandardization/DNA_Methylation.sh
+++ DataStandardization/DNA_Methylation.sh
@@ -4,13 +4,13 @@ set -e
 if [ -e $file ]
 then
 	for c in `cat CancerTypes.txt`; do
-	    DNA_Methylation.R "https://tcga.xenahubs.net/download/TCGA."$c".sampleMap/HumanMethylation450.gz" "TCGA_"$c".tsv";
+	    Rscript DNA_Methylation.R "https://tcga.xenahubs.net/download/TCGA."$c".sampleMap/HumanMethylation450.gz" "TCGA_"$c".tsv";
 	done;
 else
 	wget "https://www.dropbox.com/s/8m7ourb5ucyvcf6/GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt"
 	mv 'GPL16304-47833.txt.gz?dl=0&file_subpath=%2FGPL16304-47833.txt' $file
 	for c in `cat CancerTypes.txt`; do
-	    DNA_Methylation.R "https://tcga.xenahubs.net/download/TCGA."$c".sampleMap/HumanMethylation450.gz" "TCGA_"$c".tsv";
+	    Rscript DNA_Methylation.R "https://tcga.xenahubs.net/download/TCGA."$c".sampleMap/HumanMethylation450.gz" "TCGA_"$c".tsv";
 	done;
 fi
 rm $file
