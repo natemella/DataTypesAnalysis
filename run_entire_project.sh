b40f0a33c5896777d3c980b984caf4ff9faa4516
nathanmella <nathanmell@gmail.com>
2019-07-05 14:25:53 -0600

    Stopped using job arrays

--- run_entire_project.sh
+++ run_entire_project.sh
@@ -50,6 +50,7 @@ for i in ${index_array[@]}; do
     execute_analysis $dockerCommandsFile
 #    replace_last_line $dockerCommandsFile
     bash $dockerCommandsFile
+    wait
 #    sbatch --wait job_array.sh $dockerCommandsFile
     echo "########################################"
     echo RUNNING $(python3 get_analysis_name.py $(new_combo $i)) ANALYSIS COMMANDS
