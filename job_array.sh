829cd0c9ff4a727481e3f8d2e10d585ace06c6c3
nathanmella <nathanmell@gmail.com>
2019-07-05 17:03:46 -0600

    Stopped using job arrays

--- job_array.sh
+++ job_array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -N 1 -n 1 --mem=16G -C rhel7
+#SBATCH -N 1 -n 1 --mem=64G -C rhel7
 #SBATCH --array=0-490
 #SBATCH --mail-user=nathanmell@gmail.com   # email address
 #SBATCH --mail-type=END
