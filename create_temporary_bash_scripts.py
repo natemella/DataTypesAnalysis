588fa73162ecd4cf69f1bebbafbe6baf33e99e7e
nathanmella <nathanmell@gmail.com>
2019-07-15 15:51:12 -0600

    Running DNN with GPU

--- create_temporary_bash_scripts.py
+++ create_temporary_bash_scripts.py
@@ -215,7 +215,7 @@ for c in allDataToProcess:
             out = """#!/bin/bash
 
 #SBATCH --time=20:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=24   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH -C 'rhel7'   # features syntax (use quotes): -C 'a&b&c&d'
 #SBATCH --mem-per-cpu=79872M   # memory per CPU core
