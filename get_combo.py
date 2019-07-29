efd8aacbafff4e4fbadda25e91d5b2805de92cf9
nathanmella <nathanmell@gmail.com>
2019-07-29 09:49:35 -0600

    Changed outfile to check

--- get_combo.py
+++ get_combo.py
@@ -24,7 +24,7 @@ previous_combo = ' '.join(sys.argv[1:])
 analysis = get_name(sys.argv[1:])
 #calculate winning combination
 input_file = get_analysis_file(results_dir, analysis)
-df = pd.read_csv(input_file, sep="\t").to_numeric()
+df = pd.read_csv(input_file, sep="\t").to_numeric
 # Use double groupby in order to get the average for each iterations
 df = df.groupby(['CancerType','Algorithm','Description', 'Iteration'], sort=True).mean()
 df = df.groupby(['CancerType','Algorithm','Description'], sort=True).mean()
