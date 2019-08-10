31c0c5ac283a60df053fc3891d490b747aa1493f
nathanmella <nathanmell@gmail.com>
2019-08-09 18:00:41 -0600

    Lowered the memory and core allocation

--- create_temporary_bash_scripts.py
+++ create_temporary_bash_scripts.py
@@ -51,7 +51,7 @@ parser.add_argument(
     "-c",
     "--cores",
     type=int,
-    default=24,
+    default=5,
     help="Number of cores to allocate to docker."
 )
 parser.add_argument(
@@ -206,10 +206,10 @@ for c in allDataToProcess:
             out = """#!/bin/bash
 
 #SBATCH --time=20:00:00   # walltime
-#SBATCH --ntasks=24   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=5   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH -C 'rhel7'   # features syntax (use quotes): -C 'a&b&c&d'
-#SBATCH --mem=128G   # memory 
+#SBATCH --mem=32G   # memory 
 
 
 cd ../
