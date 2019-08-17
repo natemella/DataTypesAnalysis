4ffac9470462bdc18baff85174421bdba843cf52
nathanmella <nathanmell@gmail.com>
2019-08-17 16:39:17 -0600

    fixed bug with how we determine next combination

--- get_combo.py
+++ get_combo.py
@@ -23,12 +23,11 @@ results_dir = os.path.join(*[cwd,"Permanent_Results"])
 previous_combo = ' '.join(sys.argv[2:])
 analysis = get_name(sys.argv[2:])
 algo = sys.argv[1]
-count = previous_combo.replace("True","")
-print(count)
+count = len([item for item in sys.argv[2:] if item != "True" and item != sys.argv[2]])
 algo_nick_name = algo.split("__")[-1]
 #calculate winning combination
-input_file = get_analysis_file(results_dir, analysis)
-# input_file = f'combination_of_{count}.tsv'
+# input_file = get_analysis_file(results_dir, analysis)
+input_file = f'combination_of_{count}.tsv'
 df = pd.read_csv(input_file, sep="\t")
 df.AUROC = pd.to_numeric(df.AUROC)
 df = df.loc[df.Algorithm == algo_nick_name]
